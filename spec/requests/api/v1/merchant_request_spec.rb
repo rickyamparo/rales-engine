0ae1f2649a5af7a27b129aeb132ed83bde489e99
Brandon Stewart <boveus@gmail.com>
2017-10-02 21:26:27 -0600

    Add initial merchant routes and test for them.

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  context "GET /merchants" do
+    it "sends a list of merchants" do
+      3.times do
+        Merchant.create(name: "Timmy")
+      end
+
+      get '/api/v1/merchants'
+
+      merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchants.count).to eq(3)
+    end
+  end
+end
