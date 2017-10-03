656d89b54465f5f4b7d5b39dc391eb91224aef19
Brandon Stewart <boveus@gmail.com>
2017-10-02 22:10:09 -0600

    Add test for find.  Remove traceroute from travis.

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -29,5 +29,21 @@ describe "Merchants API" do
       expect(response).to be_success
       expect(merchant['name']).to eq("Johnny")
     end
+    context "GET /merchants/find?name='Johann'"
+      it "sends data on a merchant meeting the search criteria" do
+
+        2.times do
+          Merchant.create(name: "Timmy")
+        end
+
+        Merchant.create(name: 'Johann')
+
+        get "/api/v1/merchants/find?name='Johann'"
+
+        merchant = JSON.parse(response.body)
+
+        expect(response).to be_success
+        expect(merchant['name']).to eq("Johann")
+      end
   end
 end
