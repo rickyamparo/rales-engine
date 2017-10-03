0bc89391502ec07065b20708743fe3eb9c88fc49
Brandon Stewart <boveus@gmail.com>
2017-10-03 15:52:32 -0600

    Add initial two passing tests/features for invoice API.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         resources :find, only: [:index]
         resources :find_all, only: [:index]
       end
+      resources :invoices, only: [:show, :index]
       resources :merchants, only: [:show, :index]
     end
   end
--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-require 'rails_helper'
-
 describe "Invoices API" do
     context "GET /invoices" do
       it "sends a list of invoices" do
-      customer = Customer.create(name: "Joe")
+      customer = Customer.create(first_name: 'joe', last_name: 'henry')
       merchant = Merchant.create(name: "Timmy")
+      id = 0
       3.times do
           Invoice.create(
+            id: id += 1,
             customer: customer,
             merchant: merchant,
             status: 'complete'
           )
       end
 
-      get '/api/v1/invoices'
+      get '/api/v1/invoices.json'
 
       invoices = JSON.parse(response.body)
 
@@ -26,23 +26,25 @@ describe "Invoices API" do
   end
   context "GET /invoices/:id" do
     it "sends a single invoice" do
-    customer = Customer.create(name: "Joe")
+    customer = Customer.create(first_name: 'joe', last_name: 'henry')
     merchant = Merchant.create(name: "Timmy")
+    id = 0
     3.times do
         Invoice.create(
+          id: id += 1,
           customer: customer,
           merchant: merchant,
           status: 'complete'
         )
     end
 
-    get '/api/v1/invoices'
+    get "/api/v1/invoices/#{Invoice.first.id}"
 
     invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(invoices.first['status']).to eq("complete")
-    expect(invoices.count).to eq(1)
+    expect(invoices['status']).to eq("complete")
+    expect(invoices['id']).to eq(1)
     end
   end
 end
