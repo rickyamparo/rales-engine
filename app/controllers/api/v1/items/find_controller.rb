408d8297db144562964e829fac549822b902117c
Brandon Stewart <boveus@gmail.com>
2017-10-03 17:38:59 -0600

    Refactor find controller and item model.  Let's follow this pattern.

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,21 +1,5 @@
 class Api::V1::Items::FindController < ApplicationController
   def index
-    if params["name"]
-      render json: Item.where(
-      "lower(name) = ?", params["name"].downcase)
-      .first
-    elsif params["id"]
-      render json: Item.find(params["id"])
-    elsif params["description"]
-      render json: Item.find_by(description: params["description"])
-    elsif params["unit_price"]
-      render json: Item.find_by(unit_price: params["unit_price"])
-    elsif params["merchant_id"]
-      render json: Item.find_by(merchant_id: params["merchant_id"])
-    elsif params["created_at"]
-      render json: Item.find_by(created_at: params["created_at"])
-    elsif params["updated_at"]
-      render json: Item.find_by(updated_at: params["updated_at"])
-    end
+    render json: Item.find_item(params)
   end
 end
