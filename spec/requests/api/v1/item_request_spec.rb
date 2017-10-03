740f518319a74cdc75d29790027cdb93cb28d6d5
Ricky Amparo <rickyamparo@gmail.com>
2017-10-03 16:19:53 -0600

    add test for id find and find_all actions, item_request_spec

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -1,33 +1,37 @@
 require 'rails_helper'
 
 describe "Items API" do
+
+  before(:each) do
+    id = 0
+    merchant = Merchant.create(name: "Jerry")
+    3.times do
+        Item.create(
+          id: id += 1,
+          name: "Hula Hoop",
+          merchant_id: merchant.id)
+      end
+    Item.create(
+      id: id += 1,
+      name: "Glengoolie Blue",
+      merchant_id: merchant.id)
+  end
+
   context "GET /api/v1/items"
     it "sends a list of items" do
-      merchant = Merchant.create(name: "Jerry")
-
-      3.times do
-        Item.create(name: "Hula Hoop", merchant_id: merchant.id)
-      end
-
       get '/api/v1/items'
 
       items = JSON.parse(response.body)
 
       expect(response).to be_success
       expect(items.first['name']).to eq("Hula Hoop")
-      expect(items.count).to eq(3)
+      expect(items.last['name']).to eq("Glengoolie Blue")
+      expect(items.count).to eq(4)
     end
 
     context "GET /api/v1/items/:id"
       it "sends data on an item" do
-        merchant = Merchant.create(name: "Jerry")
-        Item.create(name: "Glengoolie Blue", merchant_id: merchant.id)
-
-        3.times do
-          Item.create(name: "Hula Hoop", merchant_id: merchant.id)
-        end
-
-        get "/api/v1/items/#{Item.first.id}"
+        get "/api/v1/items/#{Item.last.id}"
 
         item = JSON.parse(response.body)
 
@@ -37,13 +41,6 @@ describe "Items API" do
 
     context "GET /items/find?name=Glengoolie%20Blue"
       it "sends data on a item meeting the search criteria" do
-        merchant = Merchant.create(name: "Jerry")
-        Item.create(name: "Glengoolie Blue", merchant_id: merchant.id)
-
-        3.times do
-          Item.create(name: "Hula Hoop", merchant_id: merchant.id)
-        end
-
         get "/api/v1/items/find?name=Glengoolie%20blue"
 
         item = JSON.parse(response.body)
@@ -52,20 +49,35 @@ describe "Items API" do
         expect(item['name']).to eq("Glengoolie Blue")
       end
 
-    context "GET /items/find?name=Hula%20Hoop"
+    context "GET /items/find_all?name=Hula%20Hoop"
       it "sends data on all items meeting the search criteria" do
-        merchant = Merchant.create(name: "Jerry")
-        Item.create(name: "Glengoolie Blue", merchant_id: merchant.id)
-
-        3.times do
-          Item.create(name: "Hula Hoop", merchant_id: merchant.id)
-        end
-
         get "/api/v1/items/find_all?name=Hula%20Hoop"
 
         items = JSON.parse(response.body)
 
         expect(response).to be_success
         expect(items.first['name']).to eq("Hula Hoop")
+      end
+
+    context "GET /items/find?id=1" do
+      it "sends data on an item meeting the search criteria" do
+        get "/api/v1/items/find?id=1"
+
+        item = JSON.parse(response.body)
+
+        expect(response).to be_success
+        expect(item['name']).to eq("Hula Hoop")
+      end
+    end
+
+    context "GET /items/find_all?id=1"
+      it "sends data on all items meeting the search criteria" do
+        get "/api/v1/items/find_all?id=1"
+
+        items = JSON.parse(response.body)
+
+        expect(response).to be_success
+        expect(items.first['name']).to eq("Hula Hoop")
+        expect(items.count).to eq(1)
       end
 end
