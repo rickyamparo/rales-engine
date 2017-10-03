ff63a4e9807fcb2c0bd079f3a0894b512ee07864
Brandon Stewart <boveus@gmail.com>
2017-10-02 21:58:38 -0600

    Add passing test for API show.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:show, :index]
     end
   end
 end
-
-
-# /api/v1/merchants.json
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -15,5 +15,19 @@ describe "Merchants API" do
       expect(merchants.first['name']).to eq("Timmy")
       expect(merchants.count).to eq(3)
     end
+  context "GET /merchants/:id"
+    it "sends data on a merchant" do
+      Merchant.create(name: 'Johnny')
+      2.times do
+        Merchant.create(name: "Timmy")
+      end
+
+      get "/api/v1/merchants/#{Merchant.first.id}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant['name']).to eq("Johnny")
+    end
   end
 end
