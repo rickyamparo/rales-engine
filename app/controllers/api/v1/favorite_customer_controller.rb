7367c8e5ec0c50e53fd1b2f57332f7617ea07ac6
Brandon Stewart <boveus@gmail.com>
2017-10-04 21:47:55 -0600

    add non-working metho

--- app/controllers/api/v1/favorite_customer_controller.rb
+++ app/controllers/api/v1/favorite_customer_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::FavoriteCustomerController < ApplicationController
   def index
+    binding.pry
+    render json: Merchant.favorite_customer(merchant_params)
+  end
 
+private
+  def merchant_params
+    params.permit(:merchant_id)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,12 @@ class Merchant < ApplicationRecord
     .joins(:invoice_items, :transactions)
     .sum("quantity * unit_price")
   end
+
+  def self.favorite_customer(filter = nil)
+    Invoice
+    .where(filter)
+    .merge(Transaction.success)
+    .joins(:invoice_items, :transactions)
+    .max('count(customer_id)')
+  end
 end
