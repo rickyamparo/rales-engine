489a4d6d4576a27f717e158db5e51cabc03bcbdd
Brandon Stewart <boveus@gmail.com>
2017-10-05 10:33:55 -0600

    refactor invoice find and find all controllers

--- app/controllers/api/v1/invoices/find_all_controller.rb
+++ app/controllers/api/v1/invoices/find_all_controller.rb
@@ -1,18 +1,11 @@
 class Api::V1::Invoices::FindAllController < ApplicationController
   def index
-    if params["status"]
-      render json: Invoice.where(
-      "lower(status) = ?", params["status"].downcase)
-    elsif params["id"]
-      render json: Invoice.where(id: params["id"])
-    elsif params["customer_id"]
-      render json: Invoice.where(customer_id: params["customer_id"])
-    elsif params["merchant_id"]
-      render json: Invoice.where(merchant_id: params["merchant_id"])
-    elsif params["created_at"]
-      render json: Invoice.where(created_at: params["created_at"])
-    elsif params["updated_at"]
-      render json: Invoice.where(updated_at: params["updated_at"])
-    end
+    render json: Invoice.find_all(invoice_params)
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:id, :customer_id, :merchant_id, :created_at, :updated_at)
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,12 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.find_invoice(params)
+    find_by(params)
+  end
+
+  def self.find_all(params)
+    where(params)
+  end
 end
