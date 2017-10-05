593f318cf0b700865197e2e1beabe08839e622b1
Brandon Stewart <brandonsstewart88@gmail.com>
2017-10-05 10:09:38 -0600

    Update most_items_controller.rb

--- app/controllers/api/v1/items/most_items_controller.rb
+++ app/controllers/api/v1/items/most_items_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::Items::MostItemsController < ApplicationController
   def index
-    render json: Item.most_items(params['quantity'])
+    render json: Item.most_items(item_params)
   end
+
+  + private
++  
++  def item_params
++    params.permit(:quantity)
++  end
 end
