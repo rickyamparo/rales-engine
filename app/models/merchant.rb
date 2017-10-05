3b0c84841bfcb99393f67d5b98a05378083a0e1d
Brandon Stewart <boveus@gmail.com>
2017-10-05 10:36:23 -0600

    refactor merchant find and find all

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,14 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
 
+  def self.find_merchant(params)
+    find_by(params)
+  end
+
+  def self.find_all(params)
+    where(params)
+  end
+
   def self.revenue(filter = nil)
     Invoice
     .where(filter)
