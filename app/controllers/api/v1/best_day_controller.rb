b36d48ec66c31610e8e524614b6d3e1dd1279691
Brandon Stewart <brandonsstewart88@gmail.com>
2017-10-05 10:08:14 -0600

    Update best_day_controller.rb

--- app/controllers/api/v1/best_day_controller.rb
+++ app/controllers/api/v1/best_day_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::BestDayController < ApplicationController
   def index
-    render json: Item.find(params[:item_id]).best_day
+    render json: Item.find(item_params).best_day
   end
+  
+ private
+  
+  def item_params
+    params.permit(:item_id)
+  end
+  
 end
