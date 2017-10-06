868e5120032b1bfe652ae09e092c7180e33d8532
Brandon Stewart <boveus@gmail.com>
2017-10-05 22:15:10 -0600

    add actual fix for item unit price issue in find controllers

--- app/controllers/api/v1/items/find_all_controller.rb
+++ app/controllers/api/v1/items/find_all_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::Items::FindAllController < ApplicationController
 
   private
   def item_params
-    params[:unit_price].delete!('.')
+    if params[:unit_price]
+      params[:unit_price].delete!('.')
+    end
     params.permit(:id, :name,
       :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
