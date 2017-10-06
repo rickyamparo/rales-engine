25565b581e9d505acd56d9f1034392c760ab3244
Ricky Amparo <rickyamparo@gmail.com>
2017-10-06 08:53:09 -0600

    refactor customer relationships spec setup

--- spec/requests/api/v1/relationship_requests/customer_relationships_request_spec.rb
+++ spec/requests/api/v1/relationship_requests/customer_relationships_request_spec.rb
@@ -5,8 +5,6 @@ describe "Customer Relationships API" do
     @merchant = Merchant.create(id: 1, name: "Toki")
     customer = Customer.create(id: 1, first_name: "Skwisgar", last_name: "Skwigelf")
     item_1 = Item.create(id:1, merchant_id: 1, name: "Wartooth")
-    item_2 = Item.create(id:2, merchant_id: 1, name: "Guitar")
-    item_3 = Item.create(id:3, merchant_id: 1, name: "Metal")
     invoice_1 = @merchant.invoices.create(
       customer: customer,
       merchant: @merchant,
@@ -19,30 +17,7 @@ describe "Customer Relationships API" do
       status: "shipped",
       created_at: '2012-04-27 14:53:59 UTC'
     )
-    invoice_3 = @merchant.invoices.create(
-      customer: customer,
-      merchant: @merchant,
-      status: "shipped",
-      created_at: '2012-05-27 14:53:59 UTC'
-    )
 
-    3.times do
-      invoice_1.invoice_items.create(
-        item_id: item_1.id,
-        quantity: 4,
-        unit_price: 50
-      )
-      invoice_2.invoice_items.create(
-        item_id: item_2.id,
-        quantity: 3,
-        unit_price: 50
-      )
-      invoice_3.invoice_items.create(
-        item_id: item_3.id,
-        quantity: 2,
-        unit_price: 50
-      )
-    end
     invoice_1.transactions.create(
       credit_card_number: '666',
       result: "success",
@@ -53,11 +28,6 @@ describe "Customer Relationships API" do
       result: "success",
       created_at: '2012-04-27 14:53:59 UTC'
     )
-    invoice_3.transactions.create(
-      credit_card_number: '666',
-      result: "success",
-      created_at: '2012-03-27 14:53:59 UTC'
-    )
   end
 
   context "GET /api/v1/customers/:id/invoices" do
@@ -67,7 +37,7 @@ describe "Customer Relationships API" do
       customer_invoices = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(customer_invoices.count).to eq(3)
+      expect(customer_invoices.count).to eq(2)
     end
   end
 
@@ -78,7 +48,7 @@ describe "Customer Relationships API" do
       customer_transactions = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(customer_transactions.count).to eq(3)
+      expect(customer_transactions.count).to eq(2)
     end
   end
 end
