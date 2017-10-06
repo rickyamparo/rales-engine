49b9dc8f87e4161762ef0c4fe2f7d74f53a35de0
Brandon Stewart <boveus@gmail.com>
2017-10-05 19:13:03 -0600

    Merge branch 'master' into invoices_relationships

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,20 +1,9 @@
 class Api::V1::TransactionsController < ApplicationController
   def index
-<<<<<<< HEAD
-    invoice_id = invoice_params[:invoice_id].to_i
-    transactions = Transaction.find_by(invoice_id: invoice_id)
-    render json: transactions
-  end
-
-  private
-  def invoice_params
-    params.permit(:invoice_id)
-=======
     render json: Transaction.all
   end
 
   def show
     render json: Transaction.find(params[:id])
->>>>>>> master
   end
 end
