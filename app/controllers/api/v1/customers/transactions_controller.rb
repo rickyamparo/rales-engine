3b68bab376efcdbdf02272637eb7e8e0bf554a2b
Brandon Stewart <brandonsstewart88@gmail.com>
2017-10-06 01:00:02 -0600

    Update transactions_controller.rb
    Remove comments.

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
   def index
-    # joins(:zip_code)
-    #  .where("zip_code = ? and trip_date IN (?)", zip_code, date)
-    #  .count
     customer = Customer.find(customer_params[:customer_id].to_i)
     render json: Transaction.joins(invoice: [:customer]).where('invoices.customer_id =?', customer.id)
   end
